6b377d7f891a5e13e7c27dbc04524c05fc0f0aa3
Liz-En <106875266+Liz-En@users.noreply.github.com>
2022-07-01 10:39:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("changehex").onclick = function () {
+  document.getElementById("changecolor").style.color = "#55fbfb";
+};
+
+document.getElementById("changecont.").onclick = function () {
+  document.getElementById("changecontent").innerHTML =
+    "Have Fun Learning to Code";
+};
+
+document.getElementById("change").onclick = function () {
+    document.getElementById("changehref").href = "https://code.ulesson.com/"
+    
+    document.getElementById("changehref").textContent = "Go to uLesson coding school"
+    document.getElementById("changehref").style.fontFamily = "The Nautigal', cursive"
+  
+}
+ 
+
+// function myFunction() { 
+    // document.getElementById('myLink').href
+        // ="https://www.geeksforgeeks.org";
+          
+    // document.getElementById("myLink")
+        // .textContent = "Go to GeeksforGeeks";
